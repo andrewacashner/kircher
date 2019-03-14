e19fa537d5f76fac0113b8c8175388e77ce5f979
Andrew A. Cashner <andrewacashner@gmail.com>
2019-03-14 13:29:47 -0400

    add chromatic pitch support, adjust arithmetic for both diatonic and chromatic calculations, add chromatic comparison, tritone detection; check note list for valid types when creating voice

--- arca/cogito.scm
+++ arca/cogito.scm
@@ -13,6 +13,28 @@
     (let ([pair (find (lambda (pair) (pred? (cdr pair) val)) alist)])
       (car pair))))
 
+(define last-less-eq
+  (lambda (n ls)
+    "Find the last element in LS where element <= N"
+    (let loop ([ls ls])
+      (if (null? (cdr ls))
+          ls
+          (let ([this (first ls)]
+                [next (second ls)])
+            (if (and (>= n this)
+                     (< n next))
+                this
+                (loop (cdr ls))))))))
+
+(define alist-values
+  (lambda (alist)
+    "Given an alist, return a list containing just the values, 
+    the cdr of each pair"
+    (fold-right (lambda (this acc) (cons (cdr this) acc)) '() alist)))
+
+; ******************************************************
+; CLASSES and METHODS
+
 (define-class 
   <note> ()
 
@@ -44,14 +66,30 @@
   ; Global lookup keys for class
   (pitchnames
     #:allocation #:class
-    #:init-value "cdefgab"
-    #:getter pitchnames)
+    #:getter pitchnames
+    #:init-value "cdefgab")
   (accid-names
     #:allocation #:class
+    #:getter accid-names
     #:init-value '((natural . #\n)
                    (flat . #\f)
-                   (sharp . #\s))
-    #:getter accid-names)
+                   (sharp . #\s)))
+  (chrom-index
+    #:allocation #:class
+    #:getter chrom-index
+    #:init-value '((0 . 0)
+                 (1 . 2)
+                 (2 . 4)
+                 (3 . 5)
+                 (4 . 7)
+                 (5 . 9)
+                 (6 . 11)))
+  (accid-adjust
+    #:allocation #:class
+    #:getter accid-adjust
+    #:init-value '((natural . 0)
+                 (flat . -1)
+                 (sharp . 1))) ; also double accidentals here and elsewhere
 
   ; Virtual slots
   (pname ; set pnum by letter symbol instead of number, access name 
@@ -65,7 +103,7 @@
                          [c (string-ref str 0)])
                     (set! (pnum note) (string-index (pitchnames note) c)))))
 
-  (accid-name ;set accid by char name instead of symbold, access char name
+  (accid-name ; set accid by char name instead of symbol, access char name
     #:allocation #:virtual
     #:init-keyword #:accid-name
     #:accessor accid-name
@@ -74,16 +112,37 @@
     #:slot-set! (lambda (note c)
                   (set! (accid note) (rassoc (accid-names note) c char=?))))
 
-  (std-pitch ; set pnum and oct from std-pitch, get std-pitch from pnum & octave
+  (pitch-dia ; set pnum and oct from pitch-dia, get pitch-dia from pnum & octave
     #:allocation #:virtual
-    #:init-keyword #:std-pitch
-    #:accessor std-pitch
+    #:init-keyword #:pitch-dia
+    #:accessor pitch-dia
     #:slot-ref (lambda (note) 
                  (+ (* 7 (oct note)) (pnum note)))
-    #:slot-set! (lambda (note std-pitch)
+    #:slot-set! (lambda (note pitch-dia)
                   (begin
-                    (set! (pnum note) (modulo std-pitch 7))
-                    (set! (oct note) (floor (/ std-pitch 7)))))))
+                    (set! (pnum note) (modulo pitch-dia 7))
+                    (set! (oct note) (floor (/ pitch-dia 7))))))
+  (pitch-chrom ; set pnum, oct, and accid from chromatic pitch and v/v
+    #:allocation #:virtual
+    #:init-keyword #:pitch-chrom
+    #:accessor pitch-chrom
+    #:slot-ref (lambda (note)
+                 (let ([octave (* 11 (oct note))]
+                       [pnum-base (assq-ref (chrom-index note) (pnum note))]
+                       [adj (assq-ref (accid-adjust note) (accid note))])
+                   (+ octave pnum-base adj)))
+    #:slot-set! (lambda (note pitch-chrom)
+                  (let* ([octave (floor (/ pitch-chrom 11))]
+                         [pnum-abs (modulo pitch-chrom 11)]
+                         [steps (alist-values (chrom-index note))]
+                         [pnum-base (last-less-eq pnum-abs steps)]
+                         [adj (- pnum-abs pnum-base)]
+                         [acc (rassoc (accid-adjust note) adj eq?)]
+                         [pnum-dia (rassoc (chrom-index note) pnum-base eq?)])
+                  (begin
+                    (set! (pnum note) pnum-dia) 
+                    (set! (oct note) octave)
+                    (set! (accid note) acc))))))
 
 ; note arithmetic
 ; not mutating given note
@@ -98,48 +157,68 @@
         #:accid-type (accid-type note)))
 
 (define-method
-  (arithmetic (fn <procedure>) (note <note>) (n <number>))
+  (arithmetic (fn <procedure>) (access <accessor>) (note <note>) (n <number>))
   "Return a new note whose pnum and oct have been set by 
   applying procedure FN to NOTE with arg N"
   (let ([new (cp note)]
-        [pitch (fn (std-pitch note) n)])
+        [pitch (fn (access note) n)])
     (begin 
-      (set! (std-pitch new) pitch)
+      (set! (access new) pitch)
       new)))
 
 (define-method
-  (+ (note <note>) (n <number>))
+  (inc (note <note>) (n <number>))
   "Increase pitch of NOTE by N"
-  (arithmetic + note n))
+  (arithmetic + pitch-dia note n))
 
 (define-method
-  (- (note <note>) (n <number>))
+  (dec (note <note>) (n <number>))
   "Decrease pitch of NOTE by N"
-  (arithmetic - note n))
+  (arithmetic - pitch-dia note n))
+
+(define-method
+  (inc-chrom (note <note>) (n <number>))
+  (arithmetic + pitch-chrom note n))
+
+(define-method
+  (dec-chrom (note <note>) (n <number>))
+  (arithmetic - pitch-chrom note n))
+
 
 ; YES mutating given note
 (define-method
-  (arithmetic! (fn <procedure>) (note <note>) (n <number>))
+  (arithmetic! (fn <procedure>) (access <accessor>) (note <note>) (n <number>))
   "Return a new note whose pnum and oct have been set by 
   applying procedure FN to NOTE with arg N"
-  (let ([pitch (fn (std-pitch note) n)])
-      (set! (std-pitch note) pitch)
+  (let ([pitch (fn (access note) n)])
+      (set! (access note) pitch)
       note))
 
 (define-method
   (inc! (note <note>) (n <number>))
   "Increase pitch of NOTE by N"
-  (arithmetic! + note n))
+  (arithmetic! + pitch-dia note n))
 
 (define-method
   (dec! (note <note>) (n <number>))
   "Decrease pitch of NOTE by N"
-  (arithmetic! - note n))
+  (arithmetic! - pitch-dia note n))
+
+(define-method
+  (inc-chrom! (note <note>) (n <number>))
+  "Increase pitch of NOTE by N"
+  (arithmetic! + pitch-chrom note n))
+
+(define-method
+  (dec-chrom! (note <note>) (n <number>))
+  "Decrease pitch of NOTE by N"
+  (arithmetic! - pitch-chrom note n))
+
 
 ; shift octaves
 ; copy
-(define-method (8va (note <note>)) (+ note 7))
-(define-method (8vb (note <note>)) (- note 7))
+(define-method (8va (note <note>)) (inc note 7))
+(define-method (8vb (note <note>)) (dec note 7))
 
 ;original
 (define-method (8va! (note <note>)) (inc! note 7))
@@ -148,41 +227,48 @@
 
 ; difference of two notes
 (define-method
-  (- (note1 <note>) (note2 <note>))
-  "Return integer difference between standard pitch of NOTE1 and NOTE2"
-  (let ([p1 (std-pitch note1)]
-        [p2 (std-pitch note2)])
-   (- p1 p2)))
+  (diff (note1 <note>) (note2 <note>))
+  "Return integer difference between diatonic pitch of NOTE1 and NOTE2"
+  (- (pitch-dia note1) (pitch-dia note2)))
+
+(define-method
+  (diff-chrom (note1 <note>) (note2 <note>))
+  "Return integer difference between chromatic pitch of NOTE1 and NOTE2"
+  (- (pitch-chrom note1) (pitch-chrom note2)))
+
 
 ; compare
 (define-method
   (cmp (fn <procedure>) (note1 <note>) (note2 <note>))
-  (let ([p1 (std-pitch note1)]
-        [p2 (std-pitch note2)])
+  (let ([p1 (pitch-chrom note1)]
+        [p2 (pitch-chrom note2)])
     (fn p1 p2)))
 
 (define-method
-  (= (note1 <note>) (note2 <note>))
+  (note=? (note1 <note>) (note2 <note>))
   (cmp = note1 note2))
 
 (define-method
-  (> (note1 <note>) (note2 <note>))
+  (note>? (note1 <note>) (note2 <note>))
   (cmp > note1 note2))
 
 (define-method
-  (< (note1 <note>) (note2 <note>))
+  (note<? (note1 <note>) (note2 <note>))
   (cmp < note1 note2))
 
 (define-method
-  (>= (note1 <note>) (note2 <note>))
+  (note>=? (note1 <note>) (note2 <note>))
   (cmp >= note1 note2))
 
 (define-method
-  (<= (note1 <note>) (note2 <note>))
+  (note<=? (note1 <note>) (note2 <note>))
   (cmp <= note1 note2))
 
-; (TODO could also calculate chromatic intervals)
-
+(define-method
+  (tritone? (note1 <note>) (note2 <note>))
+  (let* ([diff (abs (diff-chrom note1 note2))]
+         [int (modulo diff 11)])
+    (= int 6)))
 
 ; write
 (define-method
@@ -197,36 +283,31 @@
 
 (define-method
   (smei (note <note>))
-  (let ([pname (pname note)]
-        [oct (oct note)]
-        [dur (dur note)]
-        [dots (dots note)]
-        [accid (accid note)]
-        [accid-name (accid-name note)]
-        [accid-type (accid-type note)])
-    (cond [(eq? accid-type 'default)
-           (cond [(eq? accid 'natural)
-                  `(note (@ (pname ,pname)
-                            (oct ,oct)
-                            (dur ,dur)
-                            (dots ,dots)))]
-                 [else 
+  (let* ([pname  `(pname ,(pname note))]
+         [oct    `(oct ,(oct note))]
+         [dur    `(dur ,(dur note))]
+         [dots   `(dots ,(dots note))]
+         [accid-name (accid note)] 
+         [accid-char (mei-accid note)]
+         [accid-type (accid-type note)])
 
-                   `(note (@ (pname ,pname)
-                             (oct ,oct)
-                             (dur ,dur)
-                             (accid ,accid-name)))])]
-          [(eq? accid-type 'signature)
-           `(note (@ (pname ,pname)
-                     (oct ,oct)
-                     (dur ,dur)
-                     (accid.ges ,accid-name)))]
-          [(eq? accid-type 'ficta)
-           `(note (@ (pname ,pname)
-                     (oct ,oct)
-                     (dur ,dur))
-                  (accid (@ (accid ,accid-name) 
-                            (func "ficta"))))])))
+    (cond [(eq? accid-type 'default) 
+           (if (eq? accid-name 'natural)
+               ; no accidental
+               `(note (@ ,pname ,oct ,dur ,dots))
+               ; accidental as attribute
+               `(note (@ ,pname ,oct ,dur ,dots 
+                         (accid ,accid-char))))]
+
+          [(eq? accid-type 'signature) 
+           ;accid.ges as attribute
+           `(note (@ ,pname ,oct ,dur ,dots 
+                     (accid.ges ,accid-char)))]
+
+          [(eq? accid-type 'ficta) 
+           ;accidental as child element with func attribute
+           `(note (@ ,pname ,oct ,dur ,dots) 
+                  (accid (@ (accid ,accid-char) (func "ficta"))))])))
 
 (define-method
   (mei (note <note>)) 
@@ -281,13 +362,18 @@
     (if (eq? type 'ficta)
         (assq-ref alist (accid note))
         "")))
+
+(define-method
+  (mei-accid (note <note>))
+  (let ([alist '((natural . #\n)
+                 (flat . #\f)
+                 (sharp . #\s))]) 
+    (assq-ref alist (accid note))))
+
 ; TODO could put these in class as virtual slots
 ; could allow note creation directly from MEI or Lilypond input!
 
-
-
-
-            
+; ****************************************************************
 ; REST
 (define-class <rest> (<note>))
 
@@ -307,7 +393,9 @@
   (write (rest <rest>) port)
   (format port "r~d" (dur rest)))
 
+; ****************************************************************
 ; VOICE
+
 (define-class
   <voice> (<list>)
   (id 
@@ -319,22 +407,29 @@
     #:init-keyword #:name
     #:getter name)
   (note-ls
-    #:init-value '()
-    #:init-keyword #:note-ls
-    #:accessor note-ls))
+    #:init-value '())
+  (notes
+    #:allocation #:virtual
+    #:init-keyword #:notes
+    #:accessor notes
+    #:slot-ref (lambda (o) (slot-ref o 'note-ls))
+    #:slot-set! (lambda (o ls)
+                  (if (every (lambda (n) (is-a? n <note>)) ls)
+                      (slot-set! o 'note-ls ls)
+                      (throw 'invalid-notelist ls)))))
 
 (define-generic append)
 (define-method
   (append (voice <voice>) (note <note>))
-  (let* ([ls (reverse (note-ls voice))]
+  (let* ([ls (reverse (notes voice))]
          [new (reverse (cons note ls))])
-    (set! (note-ls voice) new)))
+    (set! (notes voice) new)))
 
 (define-method
   (smei (voice <voice>))
   `(voice (@ (id ,(id voice))
              (label ,(name voice)))
-          (layer ,(map smei (note-ls voice)))))
+          (layer ,(map smei (notes voice)))))
 
 (define-method
   (mei (voice <voice>))
@@ -344,7 +439,7 @@
   (ly (voice <voice>)) 
   (format #f "\\new Voice = \"~a\" {\n~a\n}\n"
           (id voice) 
-          (string-join (map ly (note-ls voice)) " ")))
+          (string-join (map ly (notes voice)) " ")))
 
 (define-method
   (write (voice <voice>) port)
@@ -354,28 +449,31 @@
 
 (define-method
   (note-ref (voice <voice>) (i <number>))
-  (list-ref (note-ls voice) i))
+  (list-ref (notes voice) i))
 
 
 (define-method
   (test-range (voice <voice>) (extreme <note>) (test <procedure>))
-  (any (lambda (note) (test note extreme)) (note-ls voice)))
+  (any (lambda (note) (test note extreme)) (notes voice)))
 
 (define-method
   (range-too-low? (voice <voice>) (extreme <note>))
-  (test-range voice extreme <))
+  (test-range voice extreme note<?))
 
 (define-method
   (range-too-high? (voice <voice>) (extreme <note>))
-  (test-range voice extreme >))
+  (test-range voice extreme note>?))
 
 (define-method
   (transpose! (voice <voice>) (interval <number>))
-  (let* ([old (note-ls voice)]
-         [new (map (lambda (note) (+ note interval)) old)])
+  (let* ([old (notes voice)]
+         [new (map (lambda (note) (inc note interval)) old)])
     (begin 
-      (set! (note-ls voice) new)
+      (set! (notes voice) new)
       voice)))
+
+; TODO add chromatic equivalent
+; add symbols for chromatic intervals (e.g. m6, a5)
 
 (define octave-interval
   (lambda (n) (* 7 n)))
@@ -411,3 +509,9 @@
         (adjust-range-down! voice))
       voice))
 
+(define-method
+  (tritone? (voice1 <voice>) (voice2 <voice>))
+  "Boolean: Are there any tritones between the two voices?"
+  (let ([ls (zip (notes voice1) (notes voice2))])
+    (any (lambda (node) (tritone? (first node) (second node))) ls)))
+; better to report back the indexes of any tritones
