f27e72f1b4e75fe82c786108898ae041c9fa3a27
Andrew A. Cashner <andrewacashner@gmail.com>
2020-02-05 08:13:59 -0500

    progress toward regroup/pivot

--- arca/test/regroup.hs
+++ arca/test/regroup.hs
@@ -1,6 +1,17 @@
--- regroup :: [[a, a, a, a], [a, a, a, a]] -> [[a, a], [a, a], [a, a], [a, a]]
+-- regroup :: [[a, a, a, a], [a, a, a, a]] -> [[a, a], [a, a], [a, a], [a, a]] -- 
 -- regroup :: [[a, a, a, a]] -> [[a][a][a][a]]
-regroup [] = []
-regroup [(a:as), (b:bs), (c:cs), (d:ds)] = [a, b, c, d] : regroup [as, bs, cs, ds]
 
+firsts :: [[a]] -> [a]
+firsts [] = []
+firsts [(x:xs)] = [x]
+firsts ((x:xs):xss) = x : firsts xss
 
+seconds :: [[a]] -> [a]
+seconds [] = []
+seconds [(x:xs)] = [head xs]
+seconds ((x:xs):xss) = head xs : seconds xss
+
+pivot :: [[a]] -> [[a]] -> [[a]]
+pivot [] _ = []
+pivot _ [] = [] 
+pivot x y = (firsts x ++ firsts y) : pivot (tail x) (tail y)
