bb5d2a4783e90c26c1f0c4a2df0bfc7baf5105a9
Andrew A. Cashner <andrewacashner@gmail.com>
2020-03-15 15:27:57 -0400

    Lectio: read text, clean up, parse, analyze syllable count and lengths

--- arca/app/Main.hs
+++ arca/app/Main.hs
@@ -10,6 +10,7 @@ module Main where
 
 import Arca
 import Arca.Base 
+import Lectio
 import Cogito
 import Scribo
 import Fortuna
@@ -18,14 +19,15 @@ import Fortuna
 main :: IO ()
 main = do
 
-    perms <- choosePerms
-
-    let 
-        vperm = fst perms
-        rperm = snd perms
-        music = compose arca Simple Long vperm Duple rperm
-    
-    putStrLn music
-    
-    -- TODO pivot/glue
-
+--    perms <- choosePerms
+--
+--    let 
+--        vperm = fst perms
+--        rperm = snd perms
+--        music = compose arca Simple Long vperm Duple rperm
+--    
+--    putStrLn music
+--    
+--    -- TODO pivot/glue
+--
+    interact cleanup
--- arca/src/Lectio.hs
+++ arca/src/Lectio.hs
@@ -4,17 +4,81 @@
 
 module Lectio where
 
-import Data.Strings
+import Data.List.Split
 
-cleanup :: String -> [String]
-cleanup text = cleanText
+-- TODO group elements in Verbum list optimally and then send data to
+-- 'compose'
+
+data Verbum = Verbum {
+    text :: String,
+    sylCount :: Int,
+    penultLength :: SylLen
+} deriving (Show, Eq, Ord)
+
+data SylLen = Unknown | Short | Long
+    deriving (Show, Enum, Eq, Ord)
+
+data WordPos = Start | Mid | Pause | Stop
+    deriving (Show, Enum, Eq, Ord)
+
+-- | Read a text and analyze it into a list of [Verbum] objects containing
+-- needed information for text setting (syllable count, penult length)
+parse :: String -> [Verbum]
+parse text = map analyze textWords where
+    textWords = words (cleanup text)
+
+analyze :: String -> Verbum
+analyze s = Verbum {
+    text = unmark s,
+    sylCount = fst syllables,
+    penultLength = snd syllables
+} where 
+    syllables = syllabify s
+
+unmark :: String -> String
+unmark s = filter (not . (`elem` "\'-")) s
+
+syllabify :: String -> (Int, SylLen)
+syllabify s = (count, penult) 
     where
-        cleanText = map removeEmpties noComments
-        noComments = map (\ s -> removeComments s) text
-        text = lines text
+        count = length syllables
+        penult = penultValue syllables
+        syllables = wordsBy (== '-') s
+
+penultValue :: [String] -> SylLen
+penultValue text 
+    | length text <= 1 = Short
+    | head (penult text) /= '\'' = Short
+    | otherwise = Long
+
+penult :: [a] -> a
+penult = head . tail . reverse
+
+-- | Ignore comments ('% ...'), special command (lines starting with '#')
+cleanup :: String -> String
+cleanup text = unlines cleanText
+    where
+        cleanText = removeSpecial $ removeEmpties noComments
+        noComments = map (\ s -> removeComments s) (lines text)
 
 removeComments :: String -> String
-removeComments str = takeWhile (/= '%') str
+removeComments = takeWhile (/= '%') 
 
 removeEmpties :: [String] -> [String]
-removeEmpties text = filter (\ s -> (not . strNull) s) text
+removeEmpties = filter (not . strNull)
+
+removeSpecial :: [String] -> [String]
+removeSpecial = filter (\ s -> head s /= '#')
+
+strNull :: String -> Bool
+strNull s = length s == 0
+
+-- | extract title line (first line of file, '# ...') if there is one)
+takeTitle :: String -> String
+takeTitle text =
+    if head firstLine == '#' 
+        then drop 2 firstLine
+        else ""
+    where
+        firstLine = head (lines text)
+
