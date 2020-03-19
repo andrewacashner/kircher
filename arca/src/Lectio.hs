f5f825c256d74be314fa1097b83fbbdf779aa8e4
Andrew A. Cashner <andrewacashner@gmail.com>
2020-03-19 16:10:42 -0400

    trying to figure out word-grouping algorithm

--- arca/src/Lectio.hs
+++ arca/src/Lectio.hs
@@ -25,7 +25,7 @@ data WordPos = Start | Mid | Pause | Stop
 -- needed information for text setting (syllable count, penult length)
 parse :: String -> [Verbum]
 parse text = map analyze textWords where
-    textWords = words (cleanup text)
+    textWords = words $ cleanup text
 
 analyze :: String -> Verbum
 analyze s = Verbum {
@@ -59,13 +59,13 @@ cleanup :: String -> String
 cleanup text = unlines cleanText
     where
         cleanText = removeSpecial $ removeEmpties noComments
-        noComments = map (\ s -> removeComments s) (lines text)
+        noComments = map (\ s -> removeComments s) $ lines text
 
 removeComments :: String -> String
 removeComments = takeWhile (/= '%') 
 
 removeEmpties :: [String] -> [String]
-removeEmpties = filter (not . strNull)
+removeEmpties = filter $ not . strNull
 
 removeSpecial :: [String] -> [String]
 removeSpecial = filter (\ s -> head s /= '#')
@@ -80,5 +80,48 @@ takeTitle text =
         then drop 2 firstLine
         else ""
     where
-        firstLine = head (lines text)
+        firstLine = head $ lines text
+
+data Phrase = Phrase {
+    phraseText :: [Verbum],
+    phraseSylCount :: Int,
+    phrasePenultLength :: SylLen
+} deriving (Show, Eq, Ord)
+
+newPhrase :: [Verbum] -> Phrase
+newPhrase ls = Phrase {
+    phraseText = ls,
+    phraseSylCount = sum $ map sylCount ls,
+    phrasePenultLength = penultLength $ last ls 
+}
+
+showText :: Phrase -> String
+showText phrase = unwords $ map text $ phraseText phrase
+
+maxSyllables :: Int
+maxSyllables = 5
+
+-- | Group list of @Verbum@ objects into phrases where total of syllables is
+-- as large as possible but less than five
+--groupWords :: Phrase -> Phrase
+--groupWords p = let c = phraseSylCount p in
+--    if c <= maxSyllables 
+--        then p 
+--        else groupWords $ newPhrase $ fst $ splitAt (c `div` 2) $ phraseText p
+--
+
+-- packBoxes max [] = []
+-- packBoxes max [x] = if x <= max then [x] else []
+-- packBoxes max (x:xs) 
+--     | x >= max = x:(packBoxes max xs)
+--     | x + (head xs) >= max = [x, head xs] ++ (packBoxes max (tail xs))
+--     | otherwise = [x, (head xs)] ++ (packBoxes max (tail xs))
+
+appendUnderMax :: (Ord a, Num a) => a -> [a] -> a -> [a]
+appendUnderMax max ls new = 
+    if (sum ls + new) <= max 
+        then ls ++ [new]
+        else ls
+
+
 
