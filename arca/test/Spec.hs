55ae357e49eee9f200db8253981a0eaa66953b59
Andrew A. Cashner <andrewacashner@gmail.com>
2021-01-11 12:40:02 -0500

    complete test to print out all vperms

--- arca/test/Spec.hs
+++ arca/test/Spec.hs
@@ -1,5 +1,10 @@
+import System.Environment
+import System.Process
+
 import Data.Vector hiding (map, (++), concat)
-import qualified Data.Vector as V (map)
+import qualified Data.Vector as V (map, indexed)
+
+import Data.List.Index as I (indexed)
 
 import Arca_musarithmica
     (arca)
@@ -15,41 +20,90 @@ vpermTable2pitches :: VpermTable -> Vector (Vector [Pitch])
 vpermTable2pitches vpermTable = pitchVector
     where
         vpermChoirs  = vperms vpermTable
-        pitchVector  = V.map (\vperm -> makePitch $ indexed vperm) vpermChoirs
+        pitchVector  = V.map (\vperm -> makePitch $ V.indexed vperm) vpermChoirs
 
         makePitch :: Vector (Int, [Int]) -> Vector [Pitch]
         makePitch v = V.map (\(i, ps) -> 
-            map (\p -> Pitch (toEnum p) (getOct i) Mn Na) ps) v
+            map (\p -> stdPitch $ RawPitch (p - 1) (getOct i) Mn Na) ps) v
             where
                 getOct :: Int -> Int
-                getOct i | i >= 0 && i < 4 = [4, 4, 3, 3] !! i
+                getOct i | i >= 0 && i < 4 = [5, 4, 3, 2] !! i
                          | otherwise = error "octave index out of bounds"
 
 vpermPrint :: Vector [Pitch] -> String
-vpermPrint vperm = 
-    "\\score { << \\new ChoirStaff << \\new Staff <<\n\
-    \\new Voice { \\voiceOne \\clef \"treble\" \\time 2/2 "
-    ++ (concat $ map pitch2ly $ vperm ! 0)
-    ++ "}\n\\new Voice { \\voiceTwo "
-    ++ (concat $ map pitch2ly $ vperm ! 1)
-    ++ "} >>\n\
-    \\new Staff << \\new Voice { \\voiceOne \\clef \"bass\" \\time 2/2 "
-    ++ (concat $ map pitch2ly $ vperm ! 2)
-    ++ "} \\new Voice { \\voiceTwo "
-    ++ (concat $ map pitch2ly $ vperm ! 3)
-    ++ "} >> >> >> }"
+vpermPrint vperm = unlines
+    ["\\score {"
+     , "  <<"
+     , "  \\new ChoirStaff"
+     , "    <<"
+     , "    \\new Staff"
+     , "      <<"
+     , "      \\new Voice {"
+     , "        \\voiceOne \\clef \"treble\" \\time 2/2 "
+     , "          " ++ ly ! 0
+     , "      }"
+     , "      \\new Voice { \\voiceTwo "
+     , "          " ++ ly ! 1
+     , "      }"
+     , "      >>"
+     , "    \\new Staff"
+     , "      <<"
+     , "      \\new Voice {"
+     , "        \\voiceOne \\clef \"bass\" \\time 2/2 "
+     , "          " ++ ly ! 2
+     , "      }"
+     , "      \\new Voice { \\voiceTwo "
+     , "          " ++ ly ! 3
+     , "      }"
+     , "      >>"
+     , "    >>"
+     , "  >>"
+     , "}"]
+    where 
+        ly = V.map (\v -> unwords $ map (\p -> pitch2ly p) v) vperm
 
 
 main :: IO ()
-main = putStrLn $ show lyvector
-    where
-        lyvector = V.map (V.map (map vpermPrint)) pitches
-        pitches = V.map (V.map (V.map (\c -> vpermTable2pitches $ colVpermTable c))) $ perms arca
+main = do
+    putStrLn "\nWriting Lilypond code..."
+    writeFile "test/test.ly" lystring
+    putStrLn "\nCalling Lilypond to make pdf..."
+    callCommand "lilypond -o test/ test/test"
+    putStrLn "\nOpening PDF..."
+    callCommand "mupdf test/test.pdf"
 
---   results     = vpermTable2pitches table
---   table       = colVpermTable column
---   column      = arkPerms ! 0 ! 0 ! 0
---   arkPerms    = perms arca
+    where   
+        lystring = unlines $ map (\v -> unlines 
+            ["\\version \"2.20.0\""
+            , "\\book {"
+            , "\\header {"
+            , "  title=\"Arca musarithmica Athanasii Kircheri Societatis Iesu MDL\""
+            , "  subtitle=\"Andreae Cashneri Universitatis Rochesterii electronice implementata MMXX\""
+            , "}" 
+            , unlines $ map (\(i, va) -> unlines 
+                    ["\\bookpart { " 
+                    , unlines $ map (\(j, vb) -> unlines 
+                                ["\\markup { \\fill-line {"
+                                 , "  \\center-column {"
+                                 , "    \"PINAX " 
+                                    ++ show (i + 1) 
+                                    ++ " COLUMN " 
+                                    ++ show (j + 1) 
+                                    ++ "\""
+                                 , "    }"
+                                 , "  }"
+                                 , "} "
+                                , unlines vb]) $ I.indexed va
+                    , "}"]) $ I.indexed v
+            , "}"]) lylist
+
+        lylist   = toList $ V.map (\v -> toList 
+            $ V.map (\v -> toList $ V.map (\v -> toList v) v) v) lyvector
+
+        lyvector = V.map (V.map (V.map (V.map vpermPrint))) pitches
+
+        pitches  = V.map (V.map 
+            (V.map (\c -> vpermTable2pitches $ colVpermTable c))) $ perms arca
 
 {-
 Arca
@@ -60,7 +114,6 @@ Arca
                     vperms: Vector (VpermChoir)
                         Vector (Vperm)
                             [Int]
-
 vperms vpermTable :: Vector (Vector [Int])
 perms arca :: Vector (Vector (Vector Column))
 -}
