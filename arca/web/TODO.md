acdb93af8e2bfdc5ec99de0d724aa72afac487ef
Andrew A. Cashner <andrewacashner@gmail.com>
2021-06-01 10:25:03 -0400

    Add MIDI playback using MIDIjs.net

---
+++ arca/web/TODO.md
@@ -0,0 +1,11 @@
+# Web app TODO
+
+- Use web interface to select either pre-configured text (current input
+  texts), or to select an unconfigured text and select mode, meter, etc.
+    - Simplest way for latter option is to have all one section
+    - Would need to write variables into the XML file (via XSLT?)
+- Output MEI instead of PDF and display using Verovio
+- Output MIDI also
+- How to run arca not as system command? (e.g., as CGI file?)
+
+
--- arca/web/index.php
+++ arca/web/index.php
@@ -18,17 +18,28 @@
 
 $inputText = $_POST['inputText'];
 
-$inputFiles = array(
+$inputFile = array(
     "Ps150"                  => "Ps-150.xml",
     "Boethius"               => "Boethius-Nubibus_atris.xml",
     "Ave_maris_stella"       => "Ave_maris_stella.xml",
     "Veni_creator_Spiritus"  => "Veni_creator_Spiritus.xml"
 );
 
-$fileBasename = $inputFiles[$inputText];
+$fileTitle = array(
+    "Ps150"                  => "Psalmi CL",
+    "Boethius"               => "Boethius, <cite>Nubibus atriis</cite>",
+    "Ave_maris_stella"       => "<cite>Ave maris stella</cite>",
+    "Veni_creator_Spiritus"  => "<cite>Veni creator Spiritus</cite>"
+);
+
+$fileBasename = $inputFile[$inputText];
 $infileName   = "input/{$fileBasename}";
 $outfileName  = "build/{$fileBasename}.pdf";
+$MIDIoutfile  = "build/{$fileBasename}.midi";
+$title        = $fileTitle[$inputText];
 
+# Run arca (XML input, Lilypond output);
+# arca in turn runs Lilypond (PDF and MIDI output)
 exec("arca-exe {$infileName} {$outfileName}");
 
 ?>
@@ -38,13 +49,26 @@ exec("arca-exe {$infileName} {$outfileName}");
     <head>
         <title>Arca musarithmica output</title>
         <meta charset="utf-8"/> 
+        <script type="text/javascript" 
+                src="//www.midijs.net/lib/midi.js"></script>
     </head>
     <body>
         <section>
-            <h1><?=$infileName?></h1>
+            <h1><?=$title?></h1>
             <h2>Composed by the Arca musarithmica</h2>
 
-            <a href="<?=$outfileName?>">Download PDF score</a> 
+            <p>
+                <button onclick="MIDIjs.play('<?=$MIDIoutfile?>');">
+                    Listen (MIDI)
+                </button>
+                <button onclick="MIDIjs.stop()">Stop playback</button>
+            </p>
+
+            <object data="<?=$outfileName?>" type="application/pdf"
+                    width=720 height=930>
+                <a href="<?=$outfileName?>">Download PDF score</a>
+            </object>
+            </div>
         </section>
     </body>
 </html>
