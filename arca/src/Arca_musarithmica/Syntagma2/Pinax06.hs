72f4d43f62ebefa8d4411fc8220ca962d4ee1dcc
Andrew A. Cashner <andrewacashner@gmail.com>
2021-07-06 11:54:32 -0400

    s2 p6 unchecked

--- arca/src/Arca_musarithmica/Syntagma2/Pinax06.hs
+++ arca/src/Arca_musarithmica/Syntagma2/Pinax06.hs
@@ -169,24 +169,64 @@ c1 = (c1v, c1r)
 -- {{{2 c1v
 c1v = [ 
         [ -- 0
+            [5, 5, 4, 3, 2, 1, 2, 1, 7, 6, 6, 6, 7],
+            [7, 7, 1, 2, 1, 5, 5, 4, 5, 4, 5],
+            [5, 5, 6, 7, 5, 4, 3, 2, 8, 2, 2, 3, 2, 2],
+            [2, 3, 7, 8, 6, 7, 8, 5, 2, 1, 2, 5]
         ],
         [ -- 1
+            [2, 3, 3, 2, 2, 8, 7, 6, 6, 5, 5, 5],
+            [7, 7, 8, 7, 7, 5, 5, 4, 4, 4, 3, 2, 3, 2],
+            [4, 5, 4, 4, 4, 3, 2, 1, 1, 7, 1, 7],
+            [7, 5, 6, 7, 7, 3, 3, 4, 4, 5, 1, 5]
         ],
         [ --2
+            [7, 1, 2, 3, 4, 8, 8, 2, 4, 3, 2, 1, 7, 1, 7, 6, 7, 1],
+            [5, 7, 6, 5, 4, 2, 5, 5, 4, 5, 5],
+            [2, 7, 1, 1, 7, 6, 7, 1, 2, 3, 3, 2, 8, 2, 3],
+            [5, 4, 3, 2, 1, 8, 6, 5, 1]
         ],
         [ --3
+            [3, 3, 2, 1, 7, 7, 1, 1, 1, 7, 6, 7],
+            [7, 7, 6, 5, 5, 5, 5, 4, 5, 5, 4, 5],
+            [5, 5, 5, 4, 3, 2, 2, 2, 3, 2, 2],
+            [3, 3, 4, 5, 5, 1, 1, 2, 5, 1, 2, 5]
         ],
-        [ --4
+        [ --4   
+            [5, 5, 5, 4, 3, 2, 8, 2, 3, 3, 3, 4, 7],
+            [7, 7, 7, 7, 5, 6, 7, 7, 1, 7, 6, 5],
+            [3, 3, 3, 2, 3, 4, 5, 3, 3, 2, 8, 2, 3],
+            [3, 3, 3, 7, 1, 7, 3, 6, 5, 4, 3]
         ],
         [ -- 5
+            [2, 3, 3, 2, 2, 5, 5, 4, 3, 2, 3, 2, 8, 2],
+            [7, 7, 7, 7, 7, 7, 7, 6, 5, 5, 5],
+            [4, 5, 4, 4, 4, 3, 3, 1, 7, 1, 5, 1, 7, 6, 7],
+            [7, 5, 6, 7, 7, 3, 3, 4, 5, 1, 5]
         ],
         [ --6
+            [1, 1, 2, 1, 7, 3, 3, 3, 5, 4, 5],
+            [5, 5, 5, 5, 5, 7, 7, 1, 7, 7, 7],
+            [3, 3, 2, 3, 2, 5, 5, 3, 3, 3, 2, 3],
+            [1, 1, 7, 1, 5, 3, 3, 6, 3, 7, 3]
         ],
         [ -- 7
+            [8, 8, 8, 7, 3, 3, 2, 7, 1, 1, 7, 1],
+            [5, 5, 4, 5, 5, 6, 6, 5, 5, 5, 5],
+            [3, 3, 2, 8, 2, 8, 8, 4, 3, 2, 3],
+            [8, 6, 6, 5, 3, 4, 4, 5, 1, 5, 1]
         ],
         [ -- 8
+            [4, 3, 2, 8, 2, 4, 3, 3, 4, 5, 4, 3, 2, 1, 7, 1, 7, 1],
+            [2, 1, 7, 6, 7, 6, 7, 6, 5, 6, 7, 1, 1, 3, 2, 7, 6, 5, 4, 5, 5],
+            [4, 3, 2, 8, 2, 5, 5, 7, 6, 5, 4, 3, 2, 8, 2, 3],
+            [7, 6, 5, 4, 4, 4, 3, 2, 1, 1, 1, 2, 3, 4, 5, 6, 5, 1]
         ],
         [ --9
+            [8, 8, 2, 7, 8, 6, 6, 5, 5, 5, 5],
+            [5, 6, 6, 5, 5, 4, 4, 4, 3, 2, 3],
+            [3, 4, 4, 4, 3, 1, 1, 7, 1, 1, 7, 1],
+            [8, 4, 4, 5, 1, 4, 4, 5, 1, 5, 1]
         ]
     ]
 -- }}}2
@@ -194,24 +234,64 @@ c1v = [
 c1r = [ 
         [ -- duple 
             [ -- 0
+                [Sb, MnD, Sm, MnD, Fs, Fs, Mn, Sb, Mn, Mn, Mn, Sb, Sb],
+                [Sb, Sm, Sm, Mn, Sb, Sb, Sb, Mn, Sb, Mn, Sb],
+                [Sb, Sm, Sm, Mn, Mn, Mn, MnD, Fs, Fs, Mn, Mn, Mn, Sb, Sb],
+                [Sb, Mn, Mn, Mn, Mn, Mn, Mn, Sb, Mn, Mn, Sb, Sb]
             ],
             [ -- 1
+                [Mn, Sm, Sm, Sm, Sm, SmD, Fs, Sm, Sm, Mn, Sb, Sb],
+                [Sb, Sm, Sm, Sm, Sm, Sm, Sm, Sm, Sm, Sm, Fs, Fs, Sb, Sb],
+                [Mn, Sm, Sm, Sm, Sm, SmD, Fs, Sm, Sm, Mn, Sb, Sb],
+                [Mn, Sm, Sm, Sm, Sm, Sm, Sm, Sm, Sm, Mn, Sb, Sb]
             ],
             [ -- 2
+                [SmD, Fs, Sm, Sm, Mn, Sm, Sm, MnD, Sm, MnD, Sm, Sm, Sm, MnD, Fs, Fs, Mn, Sb],
+                [Mn, Mn, Mn, Mn, Mn, Mn, Sb, MnD, Sm, Sb, Sb],
+                [Mn, Mn, Mn, MnD, Fs, Fs, Mn, Sm, Sm, Mn, MnD, Fs, Fs, Sb, Sb],
+                [Sb, Mn, Mn, Sb, Mn, Sb, Mn, Sb, Sb]
             ],
             [ -- 3
+                [Sb, Mn, Sm, Sm, Sm, Sm, Sm, Sm, Mn, Sb, Sb],
+                [Sb, Mn, Mn, Sm, Sm, Sm, Sm, Mn, Mn, Mn, Mn, Sb],
+                [Sb, Sm, Sm, Sb, Mn, Sm, Sm, Mn, Mn, Mn, Sb],
+                [Sb, Mn, Mn, Sm, Sm, Sm, Sm, Mn, Mn, Mn, Mn, Sb]
             ],
             [ -- 4
+                [Sb, Mn, Mn, Mn, MnD, Fs, Fs, Mn, Mn, Sb, Mn, Sb, Sb],
+                [Sb, Mn, Mn, Mn, Sm, Sm, Sb, Mn, Sb, Mn, Sb, Sb],
+                [Sb, Mn, Mn, Mn, Mn, Sb, Mn, Sb, MnD, Fs, Fs, Mn, Sb],
+                [Sb, Mn, Mn, Mn, Mn, Sb, Mn, Sb, Mn, Sb, Sb]
             ],
             [ -- 5
+                [SmR, Sm, SmD, Fs, Sm, Sm, Sm, Sm, Mn, Sm, Sm, SbD, Sm, Sm, Sb],
+                [SmR, Sm, SmD, Fs, Sm, Sm, Sm, Sm, Mn, Mn, Br, Sb],
+                [SmR, Sm, SmD, Fs, Sm, Sm, Sm, Sm, Mn, Mn, Sm, Sm, Sb, Mn, Mn, Sb],
+                [SmR, Sm, SmD, Fs, Sm, Sm, Sm, Sm, Mn, Mn, Sb, Br]
             ],
             [ -- 6
+                [SmR, Sm, Sm, Sm, Mn, Mn, MnD, Sm, Sm, Sm, Mn, Sb],
+                [SmR, Sm, Sm, Sm, Mn, Mn, MnD, Sm, Sm, Sm, Mn, Sb],
+                [SmR, Sm, Sm, Sm, Mn, Mn, MnD, Sm, Sm, Sm, Sm, Sm, Sb],
+                [SmR, Sm, Sm, Sm, Mn, Mn, MnD, Sm, Sm, Sm, Mn, Sb]
             ],
             [ -- 7
+                [Sb, MnD, Sm, Mn, Mn, Mn, Mn, Mn, Mn, Mn, Mn, Sb],
+                [Sb, Mn, Mn, Mn, Mn, Mn, Mn, Mn, Mn, Sb, Sb],
+                [Sb, Mn, Sm, Sm, Mn, Mn, Mn, Sb, Mn, Sb, Sb],
+                [Sb, MnD, Sm, Mn, Mn, Mn, Mn, Mn, Mn, Mn, Sb, Sb]
             ],
             [ -- 8
+                [MnR, MnD, Fs, Fs, Sm, Sm, Sb, Mn, Sm, Sm, Sm, Sm, Sm, Fs, Fs, Sm, Mn, Sm, Sb],
+                [MnD, Fs, Fs, Sm, Sm, Sm, Mn, Fs, Fs, Sm, Sm, Mn, Mn, Sm, Sm, Sm, Sm, Sm, Sm, Mn, Sb],
+                [SbR, MnD, Fs, Fs, MnD, Sm, Mn, MnD, Fs, Fs, Sm, Fs, Fs, Sm, Sm, Mn, Sb],
+                [MnD, Fs, Fs, Mn, Mn, MnD, Fs, Fs, Mn, Mn, Sm, Sm, Sm, Sm, Sm, Sm, Mn, Sb]
             ],
             [ -- 9
+                [Mn, Sm, Sm, Mn, Mn, Sm, Sm, Sm, Sm, Sb, Sb],
+                [Mn, Sm, Sm, Mn, Mn, Sm, Sm, Sm, Sm, Sb, Sb],
+                [Mn, Sm, Sm, Mn, Mn, Sm, Sm, Sm, Sm, Mn, Mn, Sb],
+                [Mn, Sm, Sm ,Mn, Mn, Sm, Sm, Sm, Sm, Sb, Sb]
             ]
         ]
     ]
@@ -223,24 +303,64 @@ c2 = (c2v, c2r)
 -- {{{2 c2v
 c2v = [ 
         [ -- 0
+            [3, 2, 1, 7, 1, 6, 7, 1, 1, 1, 7, 1],
+            [7, 7, 5, 5, 5, 4, 4, 3, 4, 5, 5],
+            [5, 4, 3, 2, 3, 8, 8, 8, 2, 3, 2, 8, 2, 3],
+            [3, 7, 8, 5, 3, 4, 4, 6, 6, 5, 1]
         ],
         [ -- 1
+            [3, 4, 4, 4, 4, 3, 2, 7, 7, 1, 7, 6, 7, 1],
+            [5, 6, 6, 7, 7, 1, 6, 5, 4, 5, 5],
+            [8, 8, 8, 2, 2, 5, 4, 2, 8, 2, 3],
+            [1, 4, 2, 3, 4, 5, 5, 6, 6, 5, 1]
         ],
         [ --2
+            [2, 3, 3, 2, 3, 5, 5, 5, 6, 5, 5],
+            [7, 8, 8, 8, 7, 8, 8, 8, 8, 8, 7, 8, 7, 8],
+            [5, 5, 5, 5, 5, 3, 3, 3, 4, 4, 3, 2, 3],
+            [5, 3, 1, 5, 1, 8, 8, 8, 4, 5, 1]
         ],
         [ --3
+            [5, 6, 5, 3, 5, 4, 4, 3, 3, 2, 3],
+            [1, 1, 7, 1, 7, 6, 6, 5, 5, 5, 5],
+            [3, 4, 2, 8, 3, 8, 8, 8, 8, 8, 7, 8],
+            [1, 4, 5, 6, 3, 4, 4, 1, 1, 5, 1]
         ],
         [ --4
+            [3, 3, 2, 8, 7, 8, 2, 3, 6, 5, 5],
+            [7, 7, 7, 5, 5, 5, 5, 1, 1, 7, 1, 7, 1],
+            [5, 5, 4, 3, 2, 3, 2, 8, 4, 3, 2, 3],
+            [3, 3, 7, 8, 5, 8, 7, 6, 4, 5, 1]
         ],
         [ -- 5
+            [3, 3, 3, 2, 1, 7, 1, 1, 2, 7, 1, 7, 1],
+            [7, 7, 7, 7, 5, 5, 5, 6, 6, 5, 5],
+            [5, 5, 5, 4, 3, 2, 3, 8, 4, 3, 2, 3],
+            [3, 3, 3, 7, 8, 5, 3, 4, 4, 5, 1]
         ],
         [ --6
+            [3, 3, 2, 2, 3, 4, 5, 5, 4, 3, 2, 3],
+            [5, 5, 6, 7, 7, 1, 7, 6, 5, 5, 5],
+            [3, 3, 3, 4, 4, 8, 8, 3, 3, 1, 7, 1],
+            [8, 8, 8, 7, 7, 6, 3, 4, 1, 5, 1]
         ],
         [ -- 7
+            [3, 3, 3, 2, 8, 7, 7, 2, 2, 2, 8, 7, 6, 7, 1, 1, 7, 3, 3, 3, 2, 1, 7, 1, 1, 4, 3, 2, 3],
+            [5, 5, 5, 5, 4, 5, 5, 7, 7, 7, 6, 5, 4, 3, 4, 5, 4, 5, 7, 7, 7, 7, 5, 5, 5, 6, 5, 5],
+            [5, 5, 7, 1, 1, 2, 2, 1, 7, 6, 5, 5, 5, 5, 4, 3, 2, 3, 1, 2, 7, 1, 7, 1],
+            [8, 8, 8, 7, 6, 5, 5, 5, 3, 3, 3, 7, 8, 5, 3, 4, 4, 5, 1]
         ],
         [ -- 8
+            [5, 6, 5, 4, 3, 4, 5, 4, 8, 4, 3, 2, 3],
+            [8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8],
+            [3, 4, 3, 6, 5, 6, 5, 6, 6, 5, 4, 5],
+            [1, 4, 8, 4, 1, 4, 3, 4, 4, 1, 1]
         ],
         [ --9
+            [3, 3, 2, 1, 7, 1, 1, 7, 6, 5, 5, 3, 5, 4, 3, 7, 1, 7, 3, 2, 7, 1, 7, 1],
+            [7, 7, 7, 5, 5, 3, 4, 5, 4, 3, 2, 3, 7, 7, 7, 5, 5, 3, 4, 5, 6, 5, 5],
+            [5, 5, 4, 3, 2, 8, 8, 3, 1, 2, 7, 1, 7, 1, 5, 3, 2, 3, 2, 8, 3, 8, 4, 3, 2, 3],
+            [3, 3, 7, 8, 5, 6, 6, 3, 4, 5, 1, 3, 3, 7, 8, 5, 6, 6, 3, 4, 5, 1]
         ]
     ]
 -- }}}2
@@ -248,24 +368,64 @@ c2v = [
 c2r = [ 
         [ -- duple 
             [ -- 0
+                [Sb, Mn, Mn, Mn, Mn, SmD, Fs, Sm, Sm, Mn, Mn, Sb],
+                [Sb, Mn, Mn, Mn, Mn, Sm, Sm, SmD, Fs, Sb, Sb],
+                [Sb, Mn, Mn, Mn, Mn, Sm, Sm, SmD, Fs, Fs, Fs, Fs, Mn, Sb],
+                [Sb, Mn, Mn, Mn, Mn, Sm, Sm, Sm, Sm, Sb, Sb]
             ],
             [ -- 1
+                [Sb, Sm, Sm, Sm, Sm, MnD, Sm, Sm, Sm, MnD, Fs, Fs, Mn, Sb],
+                [Sb, Sm, Sm, Sm, Sm, MnD, Sm, MnD, Sm, Sb, Sb],
+                [Sb, Sm, Sm, Sm, Sm, MnD, Sm, Mn, Mn, Sb, Sb],
+                [Sb, Mn, Mn, MnD, Sm, Sm, Sm, Sm, Sm, Sb, Sb]
             ],
             [ -- 2
+                [Sb, Mn, Mn, Sb, Sb, SmD, Fs, Sm, Sm, Sb, Sb],
+                [Sb, Mn, Mn, Mn, Mn, Sb, SmD, Fs, Sm, Sm, Sm, Mn, Sm, Sb],
+                [Sb, Mn, Mn, Sb, Sb, SmD, Fs, Sm, Sm, Sm, Sm, Mn, Sb],
+                [Sb, Mn, Mn, Sb, Sb, SmD, Fs, Sm, Sm, Sb, Sb]
             ],
             [ -- 3
+                [Sb, Mn, Mn, Mn, Mn, Sm, Sm, Sm, Sm, Sb, Sb],
+                [Sb, Mn, Mn, Mn, Mn, Sm, Sm, Sm, Sm, Sb, Sb],
+                [Sb, Mn, Mn, Mn, Mn, Sm, Sm, Sm, Sm, Mn, Mn, Sb],
+                [Sb, Mn, Mn, Mn, Mn, Sm, Sm, Sm, Sm, Sb, Sb]
             ],
             [ -- 4
+                [Mn, Sm, Sm, Mn, Mn, SmD, Fs, Sm, Sm, Sb, Sb],
+                [Mn, Sm, Sm, Mn, Mn, SmD, Fs, Sm, Sm, Sm, Mn, Sm, Sb],
+                [Mn, Sm, Sm, Mn, Mn, SmD,Fs, Sm, Mn, Sm, Mn, Sb],
+                [Mn, Sm, Sm, Mn, Mn, SmD, Fs, Sm, Sm, Sb, Sb]
             ],
             [ -- 5  
+                [SmR, Sm, Sm, Sm, Sm, Sm, Mn, Mn, Sm, Sm, Sm, Mn, Sm, Sb],
+                [SmR, Sm, Sm, Sm, Sm, Sm, Mn, Mn, Sm, Sm, Sb, Sb],
+                [SmR, Sm, Sm, Sm, Sm, Sm, Mn, Mn, Sm, Mn, Sm, Mn, Sb],
+                [SmR, Sm, Sm, Sm, Sm, Sm, Mn, Mn, Sm, Sm, Sb, Sb]
             ],
             [ -- 6
+                [Sm, Fs, Fs, Sm, Sm, MnD, Fs, SmD, Fs, Sm, Sm, Mn, Sb],
+                [Sm, Fs, Fs, Sm, Sm, Mn, Mn, Sm, Sm, Mn, Sb],
+                [Sm, Fs, Fs, Sm, Sm, Sm, Sm, Sm, Sm, MnD, Sm, Sb],
+                [Sm, Fs, Fs, Sm, Sm, Mn, Mn, Sm, Sm, Mn, Sb]
             ],
             [ -- 7
+                [SmR, SmD, Fs, Sm, Sm, Sm, Mn, Sm, SmD, Fs, Sm, SmD, Fs, SmD, Fs, Mn, Mn, Sm ,SmD, Fs, Sm, Sm, Sm, Sm, Sm, MnD, Mn, Sm, Mn, Sb],
+                [SmR, SmD, Fs, Sm, SmD, Fs, Mn, Sm, SmD, Fs, Sm, Sm, Mn, Sm, Fs, Fs, Mn, Sm, Sm, SmD, Fs, Sm, Sm, Sm, Sm, Sm, Sb, Sb, Sb],
+                [BrR, SmR, SmD, Fs, Sm, Sm, Sm, Sm, Sm, SmD, Fs, Mn, Sm, SmD, Fs, Sm, Sm, Sm, Sm, Sm, Mn, Mn, Sm, Mn, Sm, Sb],
+                [SmR, SmD, Fs, Sm, Sm, Sm, SmD, Fs, Sb, BrR, SmR, SmD, Fs, Sm, Sm, Sm, Sm, Sm, Mn, Mn, Sb, Sb]
             ],
             [ -- 8
+                [MnR, Mn, Mn, Mn, Sb, Mn, Sb, Mn, Sm, Sm, Sb, Sm, Sm, Sb],
+                [MnR, Mn, Mn, Mn, Sb, Mn, Mn, Mn, Mn, Sb, Sb, Sb],
+                [MnR, Mn, Mn, Mn, Sb, Mn, Sb, Mn, Sb, Mn, Sm, Sm, Sb],
+                [MnR, Mn, Mn, Mn, Sb, Mn, Sb, Mn, Mn, Mn, Sb, Sb]
             ],
             [ -- 9
+                [MnR, Mn, MnD, Sm, Mn, Mn, MnD, Sm, Mn, Mn, Sb, Sb, MnR, Mn, MnD, Sm, Mn, Mn, Sb, Mn, Sb, Mn, Mn, Sb, Mn, Sb],
+                [MnR, Mn, MnD, Sm, Mn, Mn, MnD, Sm, MnD, Mn, Sm, Mn, Sb, MnR, Mn, MnD, Sm, Mn, Mn, Mn, Mn, Sb, Sb, Br, Sb],
+                [MnR, Mn, MnD, Sm, Mn, Mn, Mn, Mn, Mn, Sm, Sm, Sm, Mn, Sm, Sb, MnR, Mn, MnD, Sm, Mn, Mn, Sb, Mn, Sb, Sb, Mn, Sb, Sb],
+                [MnR, Mn, MnD, Sm, Mn, Mn, MnD, Sm, Mn, Mn, Sb, Sb, MnR, Mn, MnD, Sm, Mn, Mn, MnD, Sm, Sb, Sb, Br, Sb] -- unclear
             ]
         ]
     ]
