6eb70a517a96f2778e23f3c2e39e3b0fa4a29711
Andrew A. Cashner <andrewacashner@gmail.com>
2021-09-01 11:35:18 -0400

    improvements to LCRS tree but still not there

--- arca/test/devel/stepwise.hs
+++ arca/test/devel/stepwise.hs
@@ -3,8 +3,8 @@
  -}
 
 import Data.List
--- import Debug.Trace
---    (trace)
+import Debug.Trace
+   (trace)
 
 -- * Pitch calculations
 data Pitch = Pitch {
@@ -109,48 +109,89 @@ pitchCandidates range ps = map (pitchesInRange range) ps
 
 -- * Decision trees
 -- ** Binary tree
-data BinaryTree a = Empty | Node a (BinaryTree a) (BinaryTree a)
+data Btree a = Empty | Node a (Btree a) (Btree a)
     deriving (Show)
 
-buildTree :: a -> [[a]] -> BinaryTree a
-buildTree x []              = Node x Empty Empty
-buildTree x ((y:[]):zs)     = Node x (buildTree y zs) Empty
-buildTree x ((y1:y2:[]):zs) = Node x (buildTree y1 zs) (buildTree y2 zs)
-buildTree _ _               = error "Unknown pattern match error in buildTree!"
 
-testTree :: (a -> a -> Bool) -> a -> [[a]] -> BinaryTree a
-testTree f x []                 = Node x Empty Empty
-testTree f x ((y:[]):zs) 
-    | f x y                     = Node x (testTree f y zs) Empty 
+-- | Build a binary tree from a list of the options at each level, where there
+-- are always one or two options
+btree :: a -> [[a]] -> Btree a
+btree x []              = Node x Empty Empty
+btree x ((y:[]):zs)     = Node x (btree y zs) Empty
+btree x ((y1:y2:[]):zs) = Node x (btree y1 zs) (btree y2 zs)
+btree _ _               = error "Unknown pattern match error in btree!"
+
+testBtree :: (a -> a -> Bool) -> a -> [[a]] -> Btree a
+testBtree f x []                 = Node x Empty Empty
+testBtree f x ((y:[]):zs) 
+    | f x y                     = Node x (testBtree f y zs) Empty 
     | otherwise                 = Node x Empty Empty
-testTree f x ((y1:y2:[]):zs) 
-    | f x y1 && f x y2          = Node x (testTree f y1 zs) (testTree f y2 zs)
-    | f x y1 && (not $ f x y2)  = Node x (testTree f y1 zs) Empty
-    | f x y2 && (not $ f x y1)  = Node x (testTree f y2 zs) Empty
+testBtree f x ((y1:y2:[]):zs) 
+    | f x y1 && f x y2          = Node x (testBtree f y1 zs) (testBtree f y2 zs)
+    | f x y1 && (not $ f x y2)  = Node x (testBtree f y1 zs) Empty
+    | f x y2 && (not $ f x y1)  = Node x (testBtree f y2 zs) Empty
     | otherwise                 = Node x Empty Empty
-testTree f _ _                  = error "Unknown pattern match error in testTree!"
+testBtree f _ _                  = error "Unknown pattern match error in testBtree!"
 
--- ** Left child, right sibling tree
-data LcrsTree a = LcrsEmpty | LcrsNode a (LcrsTree a) (LcrsTree a)
-    deriving (Show)
+-- | Build a left-child/right-sibling tree from a list of the options at each
+-- level, for any number of options
+tree :: [[a]] -> Btree a
+tree ((x:[]):[]) = Node x Empty Empty           -- no siblings or children
+tree ((x:xs):[]) = Node x Empty (tree [xs])     -- siblings but no children
+tree ((x:[]):ys) = Node x (tree ys) Empty       -- children but no siblings
+tree ((x:xs):ys) = Node x (tree ys) (tree [xs]) -- both 
 
-lcrsTree :: [[a]] -> LcrsTree a
-lcrsTree []          = LcrsEmpty
-lcrsTree ((x:[]):[]) = LcrsNode x LcrsEmpty LcrsEmpty           -- no siblings, no children
-lcrsTree ((x:[]):ys) = LcrsNode x (lcrsTree ys) LcrsEmpty       -- no siblings, yes children
-lcrsTree ((x:xs):[]) = LcrsNode x LcrsEmpty (lcrsTree [xs])     -- no children, yes siblings
-lcrsTree ((x:xs):ys) = LcrsNode x (lcrsTree ys) (lcrsTree [xs]) -- both 
+-- | Build a left-child/right-sibling tree from a list of the options at each
+-- level, only including options that pass a test function; the test function
+-- compares each parent to its child
+testTree :: (a -> a -> Bool) -> [[a]] -> Btree a
 
-lChildren :: LcrsTree a -> [a]
-lChildren LcrsEmpty = []
-lChildren (LcrsNode x l _) = x:(lChildren l)
+ -- no siblings or children
+testTree f ((x:[]):[]) = trace "\n[no sibs or kids]" 
+                            Node x Empty Empty
 
-lcrsPaths :: LcrsTree a -> [a]
-lcrsPaths LcrsEmpty                 = []
-lcrsPaths (LcrsNode x LcrsEmpty r) = x:(lcrsPaths r)
-lcrsPaths (LcrsNode x l _)         = x:(lcrsPaths l)
--- TODO pass a copy of the tree like a fold?
--- need to go back up the tree to previous good L child and trace next R sibling
+-- siblings but no children
+testTree f ((x:xs):[]) = trace "\n[sibs but no kids]" 
+                            Node x Empty Empty 
+
+-- children but no siblings
+testTree f ((x:[]):(y:ys):zs)                         
+    | f x y     = trace "\n[kids, no sibs; child is good]" 
+                    Node x (testTree f ((y:ys):zs)) Empty 
+    | otherwise = trace "\n[kids, no sibs; child is bad]" 
+                    Node x (testTree f ((ys):zs)) Empty
+
+-- both children and siblings
+testTree f ((x:xs):(y:ys):zs) 
+    | f x y     = trace "\n[kids, sibs; child is good]" 
+                    Node x (testTree f ((y:ys):zs)) (testTree f ((xs):(y:ys):zs))
+    | otherwise = trace "\n[kids, sibs; child is bad]" 
+                    Node x Empty Empty
+
+
+
+
+{-
+[[a]] :
+[]
+[[x]]
+[[x],[y]]
+[[x1, x2], [y1, y2], [z]]
+-}
+
+-- ** Traversal
+preorder :: Btree a -> [a]
+preorder Empty = []
+preorder (Node x l r) = x:(preorder l) ++ (preorder r)
+
+treePaths :: Btree a -> [[a]] -> [[a]]
+treePaths Empty xs                      = map reverse xs
+treePaths (Node n l r) []               = treePaths l [[n]] ++ treePaths r []
+-- treePaths (Node n Empty r) ((x:xs):ys)  = ((n:x:xs):ys) ++ treePaths r ((x:xs):ys)
+treePaths (Node n l r) ((x:xs):ys)      = treePaths l ((n:x:xs):ys) 
+                                            ++ treePaths r ((n:x:xs):ys)
+
+-- each node returns its list of paths
 
 -- * Process a set of pitches
 
@@ -158,29 +199,15 @@ lcrsPaths (LcrsNode x l _)         = x:(lcrsPaths l)
 newPitchSet :: [Char] -> [Pitch]
 newPitchSet = map (\p -> Pitch p 0) 
 
-pitchTree :: (Pitch -> Pitch -> Bool) -> [[Pitch]] -> BinaryTree Pitch
-pitchTree f ((x:xs):ys) = testTree f x ((x:xs):ys) -- duplicate first pitch as tree root
+-- pitchTree :: (Pitch -> Pitch -> Bool) -> [[Pitch]] -> Btree Pitch
+-- pitchTree f ((x:xs):ys) = testTree f ((x:xs):ys) -- duplicate first pitch as tree root
 
-stepwise :: [[Pitch]] -> BinaryTree Pitch
-stepwise = pitchTree legalLeap
+stepwise :: [[Pitch]] -> Btree Pitch
+stepwise = testTree legalLeap
 
--- ** From tree to list of pitches
-leftChildren :: BinaryTree a -> [a]
-leftChildren Empty        = []
-leftChildren (Node x l _) = x:(leftChildren l)
-
-rightChildren :: BinaryTree a -> [a]
-rightChildren Empty        = []
-rightChildren (Node x _ r) = x:(rightChildren r)
-
-allChildren :: BinaryTree a -> [[a]]
-allChildren (Node x Empty _)     = []
-allChildren (Node x l Empty)     = [x:(leftChildren l), x:(rightChildren l)]
-allChildren (Node x l r)         = (allChildren $ Node x l Empty) 
-                                    ++ (allChildren $ Node x Empty r)
-
-pitchPaths :: BinaryTree Pitch -> [[Pitch]]
-pitchPaths tree = map tail $ allChildren tree
+-- ** From tree to list of full paths
+-- pitchPaths :: Btree Pitch -> [[Pitch]]
+-- pitchPaths tree = map tail $ allChildren tree
 
 -- * MAIN
 main :: IO()
@@ -191,6 +218,5 @@ main = do
         sopranoRange = Range (Pitch 'B' 3) (Pitch 'D' 5)
         music        = pitchCandidates sopranoRange $ newPitchSet notes
         musicTree    = stepwise music
-        paths        = pitchPaths musicTree
 
-    putStrLn $ show paths
+    putStrLn $ show musicTree
