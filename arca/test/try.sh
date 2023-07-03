90bef52866127b58bd563518f6d9c8321e154a9e
Andrew A. Cashner <andrewacashner@gmail.com>
2023-07-03 16:55:29 -0400

    verovio resourcw

--- arca/test/try.sh
+++ arca/test/try.sh
@@ -5,8 +5,8 @@ in="$1"
 out="$2"
 
 arca "$in" "$out"
-verovio --all-pages "$out"
+verovio -r /opt/local/share/verovio --all-pages "$out"
 inkview "${out%.mei}"*.svg &
-verovio -t midi "$out"
+verovio -r /opt/local/share/verovio -t midi "$out"
 env PULSE_LATENCY_MSEC=80 timidity "${out%.mei}.mid"
 
